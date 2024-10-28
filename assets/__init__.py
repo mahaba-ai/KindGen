76b1f89830f0158cf7a5a3ff5d5807e9eabd7d21
Jonah Ramponi <jonah.ramponi@smartr.ai>
2024-10-28 20:57:14 +0000

    feat: update asset path

--- assets/__init__.py
+++ assets/__init__.py
@@ -1,6 +1,6 @@
 KINDGEN_GREEN = "#374F59"
 KINDGEN_BLUE = "#0C344D"
 
-LOGO_PATH = "interface/assets/kindgen_logo_nobg.png"
-USER_AVATAR_PATH = "interface/assets/teacher.jpg"
-ASSISTANT_AVATAR_PATH = "interface/assets/paul_cable.jpeg"
+LOGO_PATH = "assets/kindgen_logo_nobg.png"
+USER_AVATAR_PATH = "assets/teacher.jpg"
+ASSISTANT_AVATAR_PATH = "assets/paul_cable.jpeg"
